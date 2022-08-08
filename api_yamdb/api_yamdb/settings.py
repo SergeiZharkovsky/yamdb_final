826a8eff5907b1ada2a3d43c2a06677e04319aca
Сергей Жарковский <sergeizharkovsky@yandex.ru>
2022-08-09 00:26:07 +0300

    Внес правки в настройки

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='123456789')
 
-DEBUG = os.getenv('DEBUG ')
+DEBUG = os.getenv('DEBUG ', default=False)
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1').split(' ')
 
